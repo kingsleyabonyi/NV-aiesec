d08085b69f52725fd3dcfd6b0a8242f96ac5244a
Kingsley <kinabonyi@gmail.com>
2024-06-05 12:01:14 +0100

    checked the serializer file

--- nv_form/serializers.py
+++ nv_form/serializers.py
@@ -1,14 +1,12 @@
-from .models import User
 from rest_framework import serializers, validators
 
 
 
 
 class UserSerializer(serializers.Serializer):
-    class Meta:
-        model = User
-        # fields = ['first_name', 'last_name', 'email', 'phone_number', 'location', 'university', 'age_range', 'sdg_interest', 'attendance_type', 'academic_level', 'academic_major', 'how_hear_about_ysf', 'contact_preference', 'employment_status', 'expectations', 'shirt_size']
-        fields = '__all__'
+    
+    # fields = ['first_name', 'last_name', 'email', 'phone_number', 'location', 'university', 'age_range', 'sdg_interest', 'attendance_type', 'academic_level', 'academic_major', 'how_hear_about_ysf', 'contact_preference', 'employment_status', 'expectations', 'shirt_size']
+    fields = '__all__'
 
 
 
