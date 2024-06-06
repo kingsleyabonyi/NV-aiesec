a6cc06cd560b481c8c651e1a581aa7c28733c765
Kingsley <kinabonyi@gmail.com>
2024-06-06 08:28:47 +0100

    serializer

--- nv_form/serializers.py
+++ nv_form/serializers.py
@@ -6,9 +6,10 @@ from .models import User
 
 
 class UserSerializer(serializers.Serializer):
-    
-    fields = ['fullname', 'email_address', 'phone_number', 'gender', 'age', 'volunteering_experience', 'member_of_aiesec', 'travel_from', 'prefer_location', 'preferred_project', 'health_issues', 'please_specify','who_do_we_contact','emergency_contact','anything_else_to_know']
-    # fields = '__all__'
+    class Meta:
+        model = User
+        fields = ['fullname', 'email_address', 'phone_number', 'gender', 'age', 'volunteering_experience', 'member_of_aiesec', 'travel_from', 'prefer_location', 'preferred_project', 'health_issues', 'please_specify','who_do_we_contact','emergency_contact','anything_else_to_know']
+        # fields = '__all__'
 
 
 
