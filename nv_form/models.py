f3d139ac7ef6570902fa383c65d8755c6af1e4f0
Kingsley <kinabonyi@gmail.com>
2024-06-06 08:10:06 +0100

    adjusted model

--- nv_form/models.py
+++ nv_form/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Models):
+class User(models.Model):
     fullname = models.CharField(max_length= 200)
     email_address = models.EmailField(max_length=200)
     phone_number = models.CharField(max_length=100)
